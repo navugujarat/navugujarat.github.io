eb4d5b511cc485664ca4cd94880b390d73c52cea
NG <navugujarat@gmail.com>
2016-06-25 00:45:29 +0530

    update 24-jun

--- js/ng.js
+++ js/ng.js
@@ -5,7 +5,7 @@ $(function(){
 		$active = "";
 	}
 	
-	$nav = '<nav class="navbar navbar-default navbar-fixed-top">' +
+	var $nav = '<nav class="navbar navbar-default navbar-fixed-top">' +
             '<div class="container">' +
                 '<div class="navbar-header">' +
                     '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">' +
@@ -24,5 +24,8 @@ $(function(){
             '</div>' +
         '</nav>'
 		
-		$('body').append($nav);
+	$('body').append($nav);
+	
+	var $footer = "<div class='container'><div class='row'><div class='col-md-3'></div><div class='col-md-6'><span class='hl2'>&copy; ૨૦૧૬ નવુ ગુજરાત. </span> અહી લખેલ દરેક લેખો <a href='https://creativecommons.org/licenses/by-sa/3.0/'>CC BY-SA 3.0</a> હેઠળ પ્રકાશીત કરેલ છે. આથી, અહી આપેલ સામગ્રી નો ઉપયોગ કરતા પહેલા આપેલ લિંક પર જઈને લાયસન્સ જરૂરથી વાંચવું. જો કોઈ લેખ પર બીજાનો અધિકાર હશે તો ત્યાં અલગથી જણાવેલ લાયસન્સને અનુસરવું. </div><div class='col-md-3'></div></div></div>"
+	$('footer').append($footer);
 })
